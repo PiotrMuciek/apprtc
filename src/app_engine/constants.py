bebe5f06fa7421d0c01dd557d99a8f42cea3493e
PiotrMuciek <piotrekmuciek@gmail.com>
2022-08-28 15:40:01 +0200

    Update constants.py
    Set TURN and STUN urls

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -20,23 +20,23 @@ LOOPBACK_CLIENT_ID = 'LOOPBACK_CLIENT_ID'
 
 # Turn/Stun server override. This allows AppRTC to connect to turn servers
 # directly rather than retrieving them from an ICE server provider.
-ICE_SERVER_OVERRIDE = None
+# ICE_SERVER_OVERRIDE = None
 # Enable by uncomment below and comment out above, then specify turn and stun
-# ICE_SERVER_OVERRIDE  = [
-#   {
-#     "urls": [
-#       "turn:hostname/IpToTurnServer:19305?transport=udp",
-#       "turn:hostname/IpToTurnServer:19305?transport=tcp"
-#     ],
-#     "username": "TurnServerUsername",
-#     "credential": "TurnServerCredentials"
-#   },
-#   {
-#     "urls": [
-#       "stun:hostname/IpToStunServer:19302"
-#     ]
-#   }
-# ]
+ICE_SERVER_OVERRIDE = [
+   {
+     "urls": [
+       "turn:146.59.19.226:3478?transport=udp",
+       "turn:146.59.19.226:3478?transport=tcp"
+     ],
+     "username": "test",
+     "credential": "test123"
+   },
+   {
+     "urls": [
+       "stun:146.59.19.226:3478"
+     ]
+   }
+ ]
 
 ICE_SERVER_BASE_URL = 'https://localhost'
 ICE_SERVER_URL_TEMPLATE = '%s/v1alpha/iceconfig?key=%s'
